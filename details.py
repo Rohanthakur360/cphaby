b5b0cc72599ee3481bec6bf913280ba81880c0d6
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-06 22:03:51 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = 28324139
+api_hash = "f15584fee477679e21e781e5da28c06a"
+bot_token = "6930341690:AAGB_i7VgEWrbbnpvvbtgo8-BCT8NB2Q2Hg"
+auth_users = [6843464670]
+sudo_user = 6843464670
+log_channel = -1002107741665
+txt_channel = -1002107741665
