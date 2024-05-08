f6188c31088d58ca5f94fde5a102a9120e059149
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-08 13:35:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,10 +43,10 @@ logging.basicConfig(
 
 bot = Client(
     "bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token
-    auth_id=auth_id
+    api_id= 28528007,
+    api_hash= "38464da16c80310cabc8d13952419cf3",
+    bot_token= "6706535540:AAF27SlXPmgl2NZtC0lP_Ql9ZtPf7nMBrSk"
+    auth_id= "6329158981"
 )
 
 @bot.on_message(filters.command(["cp"]))
