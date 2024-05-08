8f7fbc64c3482dbc683f660c518cd7844a138335
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-08 13:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,10 @@ logging.basicConfig(
 # Initialize the bot
 bot = Client(
     "bot",
-    api_id=api_id,
-    api_hash=api_hash,    
-    bot_token=bot_token
+    api_id= 28528007,
+    api_hash= "38464da16c80310cabc8d13952419cf3",    
+    bot_token= "6706535540:AAF27SlXPmgl2NZtC0lP_Ql9ZtPf7nMBrSk"
+    auth_id= "6329158981"
 )
 
 # Handler for /start command
